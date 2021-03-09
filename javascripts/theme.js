3d9fecde207d50e3f37d1ad0a523fd909e96568e
Aritz DT <aritzmadariaga@deusto.es>
2021-03-09 11:44:22 +0100

    Console logs deleted in javascript file

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -1,6 +1,3 @@
-
-
-
 document.onreadystatechange = function () {
     var state = document.readyState
     if (state == 'interactive') {
@@ -12,9 +9,8 @@ document.onreadystatechange = function () {
 
 function complete() {
     let header = document.getElementById('header');
-    let logoimg = document.createElement('img');
-    logoimg.src = "/themes/reach-redmine-theme/images/logo.png";
-    logoimg.id = "reach-logo";
-    console.log(logoimg);
-    header.appendChild(logoimg);
+    let logo = document.createElement('img');
+    logo.src = "/themes/reach-redmine-theme/images/logo.png";
+    logo.id = "reach-logo";
+    header.appendChild(logo);
 }
